b0380af368c5306b584a7a15d29ef22dced5fd86
cameron garner <camerongarner9@gmail.com>
2021-01-15 13:15:22 -0500

    Delete PiggyBank directory

--- PiggyBank/src/main/java/com/lambdaschool/piggybank/repositories/CoinRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lambdaschool.piggybank.repositories;
-
-import com.lambdaschool.piggybank.models.Coin;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CoinRepository  extends CrudRepository<Coin, Long> {
-}
